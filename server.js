684a876f979ac86e67b299f2a8f8922296e283d2
Mateo <mtaycruz@gmail.com>
2021-03-14 13:08:17 -0400

    using handlebars to create the view

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
