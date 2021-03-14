97264e9aa5030393048e35caef7234af112f825a
Mateo <mtaycruz@gmail.com>
2021-03-13 19:04:24 -0500

    established backend

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
-    order: [['created_at', 'DESC']], 
+    order: [['created_at', 'DESC']],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+    ],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
       }
     ]
-  })
+   })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
         console.log(err);
