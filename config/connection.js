2bfdd6dff3c1a86a603a343d50b0d969ff5f7581
Mateo <mtaycruz@gmail.com>
2021-03-15 17:02:19 -0400

    changed database for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,10 @@ require('dotenv').config();
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
+  host: 'us-cdbr-east-03.cleardb.com',
+  USER: 'b6595048b3f49e',
+  password: 'cd2273b8',
+  DB:'heroku_53c49ef461aff88'
 });
 
 module.exports = sequelize;
