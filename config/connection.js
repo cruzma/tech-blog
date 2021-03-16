a5440b8953ab915fcb2dd2c2d3cbbfb1c72733ce
Mateo <mtaycruz@gmail.com>
2021-03-15 22:08:43 -0400

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,11 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize({
   host: 'us-cdbr-east-03.cleardb.com',
+  user: DB_USER,
+  password: DB_PW,
+  name: DB_NAME,
   dialect: 'mysql',
   port: 3306
 });
