0e900f0582c81c15786361e324e27e9f7d9e8e97
Mateo <mtaycruz@gmail.com>
2021-03-25 12:07:27 -0400

    fixed connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+let sequelize;
+
 // create connection to our db
-const sequelize = new Sequelize({
-  host: 'us-cdbr-east-03.cleardb.com',
-  user: DB_USER,
-  password: DB_PW,
-  name: DB_NAME,
-  dialect: 'mysql',
-  port: 3306
-});
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
